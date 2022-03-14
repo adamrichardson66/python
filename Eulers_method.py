8fb3ed232a7c7543808010e57d749c9ae676a618
Adam D. Richardson <97065457+adamrichardson66@users.noreply.github.com>
2022-03-14 13:27:13 -0700

    Add files via upload

--- Eulers_method.py
+++ Eulers_method.py
@@ -10,32 +10,33 @@ import matplotlib.pyplot as plt
 
 
 
-#Analytical solution
-x = np.arange(0.,5., 0.01)
-y = np.exp(-15*x)
-plt.plot(x,y)
+#Set mesh values
+#h=1/4
+#h=2/15
+h=1/1000
 
 #Set initial condition
 x0 = 0
 y0 = 1
 
-#Set mesh size
-#h = 1/4
-#h = 2/15
-h = 1/1000
+
+
+#Analytical solution
+x = np.arange(0.,5., 0.01)
+y = np.exp(-15*x)
+plt.plot(x,y)
 
 ###Set max number of iterations
 N = 5/h
-
 
 i=0
 while i < N:
 
     x1 = x0 + h
-    y1 = y0 + h*(-15*np.exp(-15*x0))
+    y1 = y0 + h*(-15*y0)
 
     plt.plot([x0,x1],[y0,y1])
-    
+    plt.draw
     x0 = x1
     y0 = y1
 
@@ -44,9 +45,12 @@ while i < N:
 
 #Generate plot
 
+title='Approximate solution using Euler\'s method with mesh size h='+str(h)
 
 #Set plot information and display plot
-plt.suptitle('Approximate solution to ODE using Euler\'s method.')
+plt.suptitle(title)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
+
+  
